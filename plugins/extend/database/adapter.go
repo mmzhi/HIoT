293e4bff3b36e16de34c3100b5e5dd42d31233a3
Harry Rong <harrykobe@gmail.com>
2021-08-29 19:57:50 +0800

    增加适配器

---
+++ plugins/extend/database/adapter.go
@@ -0,0 +1,17 @@
+package database
+
+import "github.com/fhmq/hmq/plugins/extend"
+
+type authAdapter struct {
+}
+
+type connectAdapter struct {
+}
+
+type messageAdapter struct {
+}
+
+// NewAdapter 创建一个新的适配器
+func NewAdapter() extend.IAdapter {
+
+}
